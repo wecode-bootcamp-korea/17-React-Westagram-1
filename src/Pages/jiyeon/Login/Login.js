73104e1b49803d9302394605b05f9d8fef525bf4
jenny87879 <jenny87879@gmail.com>
2021-02-04 17:58:23 +0900

    Refactor: 코드 리팩토링 완료

--- src/Pages/jiyeon/Login/Login.js
+++ src/Pages/jiyeon/Login/Login.js
@@ -7,13 +7,13 @@ class LoginJY extends React.Component {
   constructor() {
     super();
     this.state = {
-      idInput: "",
-      pwInput: "",
+      idValue: "", //idVaiue 로 수정
+      pwValue: "",
       isBtnActive: false,
     };
   }
 
-  handleIdPwInput = (event) => {
+  handleIdPwValue = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
@@ -22,47 +22,45 @@ class LoginJY extends React.Component {
       this.goToMain()
     }
   };
-
+  
   handleBtnActive = () => {
-    const { idInput, pwInput } = this.state;
-
-    const btnCondition = idInput.includes("@") && pwInput.length >= 5;
+    const { idValue, pwValue } = this.state;
+    const btnCondition = idValue.includes("@") && pwValue.length >= 5;
+    
     this.setState({
-      isBtnActive: btnCondition,
-    });
+      isBtnActive: btnCondition 
+    },() => this.handleBtnActive('입력중')); 
   };
 
   goToMain = (event) => {
     event.preventDefault();
-    const { idInput, pwInput } = this.state;
-    const btnCondition = idInput.includes("@") && pwInput.length >= 5;
+    const { idValue, pwValue } = this.state;
+    const btnCondition = idValue.includes("@") && pwValue.length >= 5;
 
     if (btnCondition) {
-      fetch("http://10.58.2.122:8000/user", {
+      fetch("http://10.58.4.186:8000/user", {
         method: "POST",
         body: JSON.stringify({
-          email: idInput,
-          password: pwInput,
-          name: "asdfg",
-          phone_number: "019-8888-1234",
+          email: idValue,
+          password: pwValue
         }),
       })
-        .then((response) => response.json())
-        .then((result) => {
-          console.log(result);
-          if (result.meassage === "SUCCESS") {
-            alert("성공");
-            localStorage.setItem("token", result.token);
-            this.props.history.push("/main-jiyeon");
-          } else {
-            alert("회원가입이 필요합니다.");
-          }
-        });
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.message === "SUCCESS") {
+          alert("성공");
+          // localStorage.setItem("token", result.token);
+          this.props.history.push("/main-jiyeon");
+        } else {
+          alert("회원가입이나 로그인, 비밀번호 확인이 필요합니다.");
+        }
+      });
     }
   };
 
   render() {
-    const { idInput, pwInput, isBtnActive } = this.state;
+    const { idValue, pwValue, isBtnActive } = this.state;
 
     return (
       <div className="Login">
@@ -70,20 +68,20 @@ class LoginJY extends React.Component {
           <div className="instagram_logo">westagram</div>
           <form onChange={this.handleBtnActive} className="login_form">
             <input
-              onChange={this.handleIdPwInput}
-              value={idInput}
+              onChange={this.handleIdPwValue}
+              value={idValue}
               type="text"
               className="id_input"
               placeholder="전화번호, 사용자 이름 또는 이메일"
-              name="idInput"
+              name="idValue"
             />
             <input
-              onChange={this.handleIdPwInput}
-              value={pwInput}
+              onChange={this.handleIdPwValue}
+              value={pwValue}
               type="password"
               className="password_input"
               placeholder="비밀번호"
-              name="pwInput"
+              name="pwValue"
             />
             <button
               onClick={this.goToMain}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
+import './index.css'
+import './Styles/common.scss';
+import './Styles/reset.scss'
 
 import  Routes  from './Routes';
 
 ReactDOM.render(
-
-    <Routes />
-,
+  <Routes />,
   document.getElementById('root')
 );
 
